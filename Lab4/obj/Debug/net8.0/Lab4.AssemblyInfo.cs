21ba288a8d842e4a260f74f4755e9042ae54d972
yunusemredincell <yunusdincel@hotmail.com>
2024-03-13 13:00:52 +0300

    footer part  and bootstrap-logo added

--- Lab4/obj/Debug/net8.0/Lab4.AssemblyInfo.cs
+++ Lab4/obj/Debug/net8.0/Lab4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11943114fac09e66c3d821e6d29e62353e5df73b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ccae1f31ba44b7e21f7344e2d26e885795d4260")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
