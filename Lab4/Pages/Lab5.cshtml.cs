2b5a98a691710db09e1ff208c9a00bf0237bc328
yunusemredincell <yunusdincel@hotmail.com>
2024-03-20 11:46:37 +0300

    create link inside.html and successfully opened

---
+++ Lab4/Pages/Lab5.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class Lab5Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Lab4/obj/Debug/net8.0/Lab4.AssemblyInfo.cs
+++ Lab4/obj/Debug/net8.0/Lab4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ccae1f31ba44b7e21f7344e2d26e885795d4260")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21ba288a8d842e4a260f74f4755e9042ae54d972")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
