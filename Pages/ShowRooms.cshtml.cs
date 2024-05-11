87734aab8ea2f6281e00e2cd7e7ba8cf816b8f61
yunusemredincell <yunusdincel@hotmail.com>
2024-05-11 13:38:49 +0300

    Style of the pages are added. Also pages' cs are added to connect to the frontend and backend sides.

--- Pages/ShowRooms.cshtml.cs
+++ Pages/ShowRooms.cshtml.cs
@@ -5,8 +5,17 @@ namespace MyApp.Namespace
 {
     public class ShowRoomsModel : PageModel
     {
+          RoomService roomService;
+        
+        private readonly AppDbContext show_context;
+        public ShowRoomsModel(AppDbContext context){
+          this.roomService = new RoomService(context);
+
+        }
+        public List<Room>RoomList { get; set; }=default!;
         public void OnGet()
         {
+              RoomList=roomService.GetRooms();
         }
     }
 }
